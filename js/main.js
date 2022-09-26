b4bc7c1dc5ca6ef1909a25e909886987f8892602
mludwig99 <michalludwig99@gmail.com>
2022-09-26 18:42:22 +0200

    update-2

--- js/main.js
+++ js/main.js
@@ -1,40 +1,47 @@
-
 const userName = document.querySelector("#username");
 const password = document.querySelector("#password");
 const password2 = document.querySelector("#password2");
 const email = document.querySelector("#email");
 const btnClear = document.querySelector(".clear");
 const btnSend = document.querySelector(".send");
-const popup = document.querySelector(".popup")
+const popup = document.querySelector(".popup");
+
+const displayError = (input, mesage) => {
+  const formBox = input.parentElement;
+    const errorMesage = formBox.querySelector(".error-text");
+    
+    formBox.classList.add('error')
+    errorMesage.textContent = mesage;
+};
 
 
-const checkForm = input => {
-    input.forEach(element => {
-        if (element.value === '') {
-            console.log('błąd');
-        
-        }
-        else {
-            console.log('ok')
-        }
-    })
+const clearError = input => {
+    const formBox = input.parentElement;
+    formBox.classList.remove('error');
 }
 
 
-   
 
+const checkForm = (input) => {
+  input.forEach((element) => {
+    if (element.value === "") {
+      displayError(element, element.placeholder);
+    } else {
+      clearError(element)
+    }
+  });
+};
 
-btnSend.addEventListener('click', e => {
-    e.preventDefault();
+btnSend.addEventListener("click", (e) => {
+  e.preventDefault();
 
+  checkForm([userName, password, password2, email]);
+});
 
-    checkForm([userName, password, password2, email])
-})
+btnClear.addEventListener("click", (e) => {
+  e.preventDefault();
 
-btnClear.addEventListener('click', e => {
-    e.preventDefault();
-  
-    [userName, password, password2, email].forEach(element =>{
-        element.value = '';
-    })
-})
+  [userName, password, password2, email].forEach((element) => {
+    element.value = "";
+  });
+});
